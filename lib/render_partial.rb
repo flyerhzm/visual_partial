1bcc3ccaf787b78ef4741342ee058178530b2cfc
flyerhzm <flyerhzm@flyerhzm-laptop.(none)>
2009-07-07 22:17:41 +0800

    add support to partial page whose top element is <tr> or %tr

--- lib/render_partial.rb
+++ lib/render_partial.rb
@@ -7,10 +7,22 @@ module ActionView
     def render_partial(view, object = nil, local_assigns = {}, as = nil)
       template_content = origin_render_partial(view, object, local_assigns, as)
       if VisualPartial.display
-        "<div style='position:absolute;background:red;z-index:99999;'>#{path_without_format_and_extension}</div>#{template_content}" 
+        if tr_partial?(template_content)
+          "<tr><td>#{partial_display}</td></tr>#{template_content}"
+        else
+          partial_display + template_content 
+        end
       else
         template_content
       end
     end 
+
+    def tr_partial?(template_content)
+      return template_content =~ /\s*<tr>/ || template_content =~ /\s*%tr/
+    end
+
+    def partial_display
+      "<div style='position:absolute;background:red;z-index:99999;opacity:.7;'>#{path_without_format_and_extension}</div>"
+    end
   end
 end
